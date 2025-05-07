8c7df3e4eb8cbeb96ec97e4a41c7a41101503423
qinflan <158785365+qinflan@users.noreply.github.com>
2025-05-07 03:16:39 -0400

    Integrate HIDD API from Espressif

--- main/ble.h
+++ main/ble.h
@@ -8,6 +8,13 @@
 #include "nvs_flash.h"
 #include "esp_bt_defs.h"
 
+// GATT
+#include "esp_gattc_api.h"
+#include "esp_gatts_api.h"
+#include "esp_gatt_common_api.h"
+#include "esp_hidd_api.h"
+
+
 #define BLE_ADV_INT_MIN 0x20
 #define BLE_ADV_INT_MAX 0x30
 #define BLE_SPAM_DEVICE_NAME "macero"
